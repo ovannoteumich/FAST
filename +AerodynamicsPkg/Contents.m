06b0563793473657dffcabdd9e486dcdbf3c2f8f
prmoko <prmoko@umich.edu>
2025-06-04 10:05:43 -0400

    Updated AeroPkg Contents
    Updated the contents held within the AerodyanmicsPkg to reflect the new code additions and name changes.

--- +AerodynamicsPkg/Contents.m
+++ +AerodynamicsPkg/Contents.m
@@ -1,12 +1,13 @@
 % Contents of AerodynamicsPkg package:
 %
-% README                  - AerodynamicsPkg.README is a function.
+% README              - AerodynamicsPkg.README is a function.
 %
 % Functions
 % -------------------------------------------------------------------------
-% CompressibilityDrag     - AerodynamicsPkg.CompressibilityDrag is a function.
-% ComputeDragCoefficients - AerodynamicsPkg.ComputeDragCoefficients is a function.
-% InducedDrag             - AerodynamicsPkg.InducedDrag is a function.
-% LiftDependentDrag       - AerodynamicsPkg.LiftDependentDrag is a function.
-% SkinFrictionDrag        - AerodynamicsPkg.SkinFrictionDrag is a function.
+% CompressibilityDrag - AerodynamicsPkg.CompressibilityDrag is a function.
+% ConstantLD          - AerodynamicsPkg.ConstantLD is a function.
+% DragPolar           - AerodynamicsPkg.DragPolar is a function.
+% InducedDrag         - AerodynamicsPkg.InducedDrag is a function.
+% LiftDependentDrag   - AerodynamicsPkg.LiftDependentDrag is a function.
+% SkinFrictionDrag    - AerodynamicsPkg.SkinFrictionDrag is a function.
 % 
