acf79d7369e9948ff8de4f3dcbb25b8b7937464b
prmoko <prmoko@umich.edu>
2025-06-05 13:05:22 -0400

    Updated InducedDrag
    Modified the input parameters to match that of an Aircraft structure and re-formatted the code.

--- +AerodynamicsPkg/InducedDrag.m
+++ +AerodynamicsPkg/InducedDrag.m
@@ -1,42 +1,48 @@
-function [CDi] = InducedDrag(Inputs)
+function [CDi] = InducedDrag(Aircraft)
 %
-% [CDi] = InducedDrag(Inputs)
+% [CDi] = InducedDrag(Aircraft)
 % modified by Paul Mokotoff, prmoko@umich.edu
 % patterned after Aviary's "compute" method in induced_drag.py,
 % translated by Cursor, an AI Code Editor
-% last updated: 28 may 2025
+% last updated: 05 jun 2025
 %
 % INPUTS:
-%     Inputs - data structure with all necessary inputs.
-%              size/type/units: 1-by-1 / struct / []
+%     Aircraft - data structure with the aircraft geometry and mission
+%                history.
+%                size/type/units: 1-by-1 / struct / []
 %
 % OUTPUTS:
-%     CDi    - induced drag coefficient.
-%              size/type/units: npnt-by-1 / double / []
+%     CDi      - induced drag coefficient.
+%                size/type/units: npnt-by-1 / double / []
 %
 
 
 %% PARSE INPUTS %%
 %%%%%%%%%%%%%%%%%%
 
-% get all inputs
-Mach = Inputs.Mach;
-Lift = Inputs.Lift;
-P = Inputs.Pressure;
-Sref = Inputs.WingArea;
-AR = Inputs.AspectRatio;
-SpanEfficiencyFactor = Inputs.SpanEfficiencyFactor;
-SW25 = Inputs.SW25;
-TR = Inputs.TR;
-Gamma = Inputs.Gamma;
-Redux = Inputs.Redux;
+% get the segment id
+SegsID = Aircraft.Mission.Profile.SegsID;
+
+% get the beginning and ending control point indices
+SegBeg = Aircraft.Mission.Profile.SegBeg(SegsID);
+SegEnd = Aircraft.Mission.Profile.SegEnd(SegsID);
+
+% get the mach number
+Mach = Aircraft.Mission.History.SI.Performance.Mach(SegBeg:SegEnd);
+CL   = Aircraft.Mission.History.SI.Aero.CL(         SegBeg:SegEnd);
+
+% get aircraft geometry
+AR    = Aircraft.Specs.Aero.Wing.AR   ;
+e     = Aircraft.Specs.Aero.Wing.e    ;
+Sweep = Aircraft.Specs.Aero.Wing.Sweep;
+TR    = Aircraft.Specs.Aero.Wing.TR   ;
+
+% check option for extreme taper ratios
+Redux = Aircraft.Specs.Aero.Wing.Redux;
 
 
 %% COMPUTE THE INDUCED DRAG COEFFICIENT %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% calculate the lift coefficient
-CL = 2.0 .* Lift ./ (Sref .* Gamma .* P .* Mach .^ 2);
 
 % check for the redux flag
 if (Redux)
@@ -44,25 +50,25 @@ if (Redux)
     % adjust for extreme taper ratios
     % Reference: DeYoung, John. "Advanced Supersonic Technology Concept Study Reference
     % Characteristics," NASA Contractor Report 132374.
-    SpanEfficiency0 = 1.0 + 0.1 * AR * (0.4226 * sqrt(AR) - 0.35 * TR - 0.143);
+    e0 = 1 + 0.1 * AR * (0.4226 * sqrt(AR) - 0.35 * TR - 0.143);
     
 else
     
     % assume a perfect efficiency scale factor
-    SpanEfficiency0 = 1.0;
+    e0 = 1;
     
 end
 
 % modify the span efficiency factor
-if (SpanEfficiencyFactor <= 0.3)
+if (e <= 0.3)
     
     % add to the existing one
-    SpanEfficiency = SpanEfficiency0 + SpanEfficiencyFactor;
+    SpanEfficiency = e0 + e;
     
 else
     
     % scale the existing one
-    SpanEfficiency = SpanEfficiency0 * SpanEfficiencyFactor;
+    SpanEfficiency = e0 * e;
     
 end
 
@@ -70,17 +76,17 @@ end
 CDi = CL .^ 2 ./ (pi * AR * SpanEfficiency);
 
 % if forward sweep, add Warner Robins Factor
-if real(SW25) < 0.0
+if (real(Sweep) < 0.0)
     
     % convert degrees to radians
-    DegToRad = pi/180;
+    DegToRad = pi / 180;
     
     % compute scale factors
-    TH = (1.0 - TR) / (1.0 + TR) / AR;
-    TanSW = tan(SW25 / DegToRad);
-    COSA = 1.0 / sqrt(1.0 + (TanSW - 3.0 * TH)^2);
-    COSB = 1.0 / sqrt(1.0 + (TanSW + TH)^2);
-    CAYT = 0.5 * ((1.1 - 0.11/(1.1 - Mach * COSA))/(1.1 - 0.11/(1.1 - Mach * COSB)) - 1.0)^2;
+    TH = (1 - TR) / (1 + TR) / AR;
+    TanSW = tan(Sweep / DegToRad);
+    COSA = 1 / sqrt(1 + (TanSW - 3 * TH)^2);
+    COSB = 1 / sqrt(1 + (TanSW + TH)^2);
+    CAYT = 0.5 * ((1.1 - 0.11 / (1.1 - Mach * COSA)) / (1.1 - 0.11 / (1.1 - Mach * COSB)) - 1.0) ^ 2;
     
     % scale the induced drag coefficient
     CDi = CDi + CAYT .* CL .^ 2;
