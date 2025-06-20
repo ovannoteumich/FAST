6c066439396dad0f78a438495718fb47ffbace5a
prmoko <prmoko@umich.edu>
2025-06-20 15:24:41 -0400

    Updated Contents Files
    Updated the files with the latest functions stored in each package.

--- +BatteryPkg/Contents.m
+++ +BatteryPkg/Contents.m
@@ -7,3 +7,6 @@
 % GroundCharge           - BatteryPkg.GroundCharge is a function.
 % Model                  - BatteryPkg.Model is a function.
 % ResizeBattery          - BatteryPkg.ResizeBattery is a function.
+% TestGroundCharge       - BatteryPkg.TestGroundCharge is a function.
+% TestModel              - BatteryPkg.TestModel is a function.
+% TestResizeBattery      - BatteryPkg.TestResizeBattery is a function.
