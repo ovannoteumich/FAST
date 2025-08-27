3c1924968b0c32c764d29995f6df75b70a9adc0f
prmoko <prmoko@umich.edu>
2025-08-27 09:39:20 -0400

    Created Boeing777
    Created a constraint diagram for a Boeing 777 that verifies the one provided in the Aircraft Design Metabook.

---
+++ +ConstraintDiagramPkg/Boeing777.m
@@ -0,0 +1,138 @@
+function [] = Boeing777()
+%
+% [] = Boeing777()
+% written by Paul Mokotoff, prmoko@umich.edu
+% last updated: 27 aug 2025
+%
+% create a constraint diagram for a boeing 777, found in the aircraft
+% design metabook.
+%
+% INPUTS:
+%     none
+%
+% OUTPUTS:
+%     none
+%
+
+% initial cleanup
+clc, close all
+
+
+%% DEFINE THE AIRCRAFT'S PARAMETERS %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                            %
+% top-level aircraft         %
+% requirements               %
+%                            %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% entry-into-service year
+Aircraft.Specs.TLAR.EIS = 2006;
+
+% aircraft class
+Aircraft.Specs.TLAR.Class = "Turbofan";
+
+% ----------------------------------------------------------
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                            %
+% performance parameters     %
+%                            %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% design range
+Aircraft.Specs.Performance.Range = 16045;
+
+% altitudes
+Aircraft.Specs.Performance.Alts.Crs = UnitConversionPkg.ConvLength(40000, "ft", "m"); % approx 4,000 ft less than A320 absolute ceiling
+Aircraft.Specs.Performance.Alts.Srv = UnitConversionPkg.ConvLength(42000, "ft", "m"); % approx 2,000 ft less than A320 absolute ceiling
+
+% cruise mach number
+Aircraft.Specs.Performance.Vels.Crs = 0.84;
+
+% runway lengths and obstacle clearances
+Aircraft.Specs.Performance.TOFL    = UnitConversionPkg.ConvLength(12000, "ft", "m");
+Aircraft.Specs.Performance.LFL     = UnitConversionPkg.ConvLength(12000, "ft", "m");
+Aircraft.Specs.Performance.ObstLen = UnitConversionPkg.ConvLength( 1000, "ft", "m");
+
+% multiplicative factors for OEI conditions
+Aircraft.Specs.Performance.TempInc = 1.25;
+Aircraft.Specs.Performance.MaxCont = 1 / 0.94;
+
+% design specific excess power loss ???
+Aircraft.Specs.Performance.PsLoss = 0.6238;
+
+% ----------------------------------------------------------
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                            %
+% aerodynamic parameters     %
+%                            %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% wing loading
+Aircraft.Specs.Aero.W_S.SLS = 695.5;
+
+% wing properties
+Aircraft.Specs.Aero.S  = 347815 / 695.5;
+Aircraft.Specs.Aero.AR = 9.8;
+
+% lift coefficients
+Aircraft.Specs.Aero.CL.Crs = 0.9;
+Aircraft.Specs.Aero.CL.Tko = 2.0;
+Aircraft.Specs.Aero.CL.Lnd = 2.6;
+
+% parasite drag coefficients
+Aircraft.Specs.Aero.CD0.Tko = 0.06097;
+Aircraft.Specs.Aero.CD0.Crs = 0.01597;
+Aircraft.Specs.Aero.CD0.Lnd = 0.11597;
+
+% Oswald efficiency factors
+Aircraft.Specs.Aero.e.Crs = 0.8514;
+Aircraft.Specs.Aero.e.Tko = 0.8012;
+Aircraft.Specs.Aero.e.Lnd = 0.7512;
+
+% ----------------------------------------------------------
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                            %
+% weights                    %
+%                            %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% MTOW
+Aircraft.Specs.Weight.MTOW = 347815;
+
+% ----------------------------------------------------------
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                            %
+% propulsion system          %
+% specifications             %
+%                            %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% thrust-weight ratio
+Aircraft.Specs.Propulsion.T_W.SLS = 0.2888;
+
+% number of engines
+Aircraft.Specs.Propulsion.NumEngines = 2;
+
+
+%% RUN THE CONSTRAINT ANALYSIS %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% determine which constraints to use (0 = 14 CFR 25; 1 = novel)
+Aircraft.Settings.ConstraintType = 0;
+
+% create a constraint diagram
+ConstraintDiagramPkg.ConstraintDiagram(Aircraft);
+
+% add the existing sizing point
+hold on
+scatter(Aircraft.Specs.Aero.W_S.SLS, Aircraft.Specs.Propulsion.T_W.SLS, 48, "o", "MarkerEdgeColor", "red", "MarkerFaceColor", "red");
+
+
+end
