aa3a4b142e475174ccd1293d1cee3773100cd728
Maxfield Arnson <112970903+marnson@users.noreply.github.com>
2025-09-16 16:53:05 -0400

    linting a little bit

--- +ThermalPkg/BuildThermArch.m
+++ +ThermalPkg/BuildThermArch.m
@@ -1,9 +1,9 @@
 % function [] = BuildThermArch()
 clear;clc;
-
-
 % Inputs
 
+
+% would pull these from FAST
 % Simple PHE from Paul's Paper; hardcoded for testing
 PropSrcType = [1; 0];
 PropTrnType = [1; 0; 2];
@@ -14,13 +14,11 @@ PropTrnType = PropTrnType + 2;
 % 0 is batt, 2 is motor, 5 is generator
 HeatSrc = [PropSrcType(PropSrcType == 0); PropTrnType(PropTrnType == 2); PropTrnType(PropTrnType == 5)];
 
-
 % Group by type
 HeatGroups = unique(HeatSrc);
 
 % always 2 sinks, internal reservoir and ambient, and each can have a pump
 UpStreamStem = zeros(length(HeatGroups) + 2 + 2);
-
 UpStreamStem(end-3 ,end-1) = 1; % Connect Sink to its pump
 UpStreamStem(end-2 ,end) = 1; % Connect Sink to its pump
 
@@ -29,8 +27,6 @@ items = num2str(1:length(HeatGroups));
 items = items(items~= ' ');
 items = num2cell(items);
 LoopsUnordered = ThermalPkg.Partitions(items);
-
-% LoopsUnordered = LoopsUnordered(2)
 
 
 UpStreamStruct = ThermalPkg.BuildUpStreams(LoopsUnordered,UpStreamStem)
