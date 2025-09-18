a6d97c264adbe3cf86a4c9ee111d1b15686c56e8
Maxfield Arnson <112970903+marnson@users.noreply.github.com>
2025-09-18 17:03:57 -0400

    Update deletethisifyouseeit.m

--- deletethisifyouseeit.m
+++ deletethisifyouseeit.m
@@ -1,29 +1,29 @@
-
-%% A320
-clear; clc; close all;
-
-tic
-AC_In = AircraftSpecsPkg.A320Neo;
-AC_In.Settings.Plotting = 0;
-
-AC_Out = Main(AC_In,@MissionProfilesPkg.A320)
-toc
-
-AC_Out.Specs.Weight
-
-
-
-%% CeRAS
-clear; clc; close all;
-
-tic
-AC_In = AircraftSpecsPkg.CeRAS;
-AC_In.Settings.Plotting = 0;
-
-AC_Out = Main(AC_In,@MissionProfilesPkg.CeRAS)
-toc
-
-AC_Out.Specs.Weight
+% 
+% %% A320
+% clear; clc; close all;
+% 
+% tic
+% AC_In = AircraftSpecsPkg.A320Neo;
+% AC_In.Settings.Plotting = 0;
+% 
+% AC_Out = Main(AC_In,@MissionProfilesPkg.A320)
+% toc
+% 
+% AC_Out.Specs.Weight
+% 
+% 
+% 
+% %% CeRAS
+% clear; clc; close all;
+% 
+% tic
+% AC_In = AircraftSpecsPkg.CeRAS;
+% AC_In.Settings.Plotting = 0;
+% 
+% AC_Out = Main(AC_In,@MissionProfilesPkg.CeRAS)
+% toc
+% 
+% AC_Out.Specs.Weight
 
 %% AEA
 
