284ad4b4e5c4303d769860afa659a9292f83bccf
Maxfield Arnson <112970903+marnson@users.noreply.github.com>
2025-11-19 09:37:54 -0500

    adding figures

--- +ThermalPkg/AviationPaperAbstractFigs.m
+++ +ThermalPkg/AviationPaperAbstractFigs.m
@@ -4,22 +4,22 @@ clear; clc; close all;
 % Custom architecture for simple trade study
 PropArch.SrcType = [1];
 % 0 is EM, 1 is eng, 2 is prop, 3 is generator
-PropArch.TrnType = [0 0 2 2 3];
+PropArch.TrnType = [0 0 2 2];
 
 % Create Thermal Architectures from propulsive
 [ArchCells] = ThermalPkg.ArchitecturePkg.ThermalFromProp(PropArch);
 
 % Test a chosen architecture
-testno = 7;
-ThermalSystem = ArchCells.("Arch_" + testno);
+testno = 4;
+ThermalSystemP = ArchCells.("Arch_" + testno);
 
 % Visualize our current architecture
 figure(1)
 
-CustomNames = {'Heat Source 1';'Heat Source 2';'Heat Pump A';'Heat Pump B';'Sink A';'Sink B'};
+CustomNames = {'Heat Source';'Heat Pump B';'Heat Pump A';'Sink B';'Sink A'};
 
 
-simplegraph = digraph(ThermalSystem.Arch,CustomNames);
+simplegraph = digraph(ThermalSystemP.Arch,CustomNames);
 
 % Plot the digraph
 p2 = plot(simplegraph);
@@ -35,27 +35,28 @@ ax = gca;
 ax.FontName = 'times';
 ax.Visible = 'off';
 
-%% arch 2
-clear; clc; close all;
+
+% ---------------------------------------
+% Active System Setup
+% ---------------------------------------
 
 % Custom architecture for simple trade study
 PropArch.SrcType = [1];
 % 0 is EM, 1 is eng, 2 is prop, 3 is generator
-PropArch.TrnType = [0 0 2 2 3];
+PropArch.TrnType = [0 0 2 2];
 
 % Create Thermal Architectures from propulsive
 [ArchCells] = ThermalPkg.ArchitecturePkg.ThermalFromProp(PropArch);
 
 % Test a chosen architecture
-testno = 13;
-ThermalSystem = ArchCells.("Arch_" + testno);
+testno = 2;
+ThermalSystemA = ArchCells.("Arch_" + testno);
 
 % Visualize our current architecture
 figure(2)
-CustomNames = {'Heat Source 1';'Heat Source 2';'Heat Pump A';'Heat Pump B';'Sink A';'Sink B'};
 
 
-simplegraph = digraph(ThermalSystem.Arch,CustomNames);
+simplegraph = digraph(ThermalSystemA.Arch,CustomNames);
 
 % Plot the digraph
 p2 = plot(simplegraph);
@@ -70,6 +71,101 @@ hold on
 ax = gca;
 ax.FontName = 'times';
 ax.Visible = 'off';
+
+
+% ---------------------------------------
+% Trade Studies
+% ---------------------------------------
+
+
+load('+ThermalPkg\+MaterialsPkg\Material_DB.mat')
+ThermalSystemP.WorkingFluid = FluidProperties.EthyleneGlycol_30;
+ThermalSystemA.WorkingFluid = FluidProperties.EthyleneGlycol_30;
+
+
+% Pull In from propulsion system (need this or a specified heat generation)
+
+ThermalSystemP.Propulsion.Efficiency = ...
+    0 * ones(length(ThermalSystemP.CompNames),1); % Fake numbers for now
+ThermalSystemA.Propulsion.Efficiency = ...
+    0 * ones(length(ThermalSystemP.CompNames),1); % Fake numbers for now
+
+
+
+
+% Temp settings, design variables or from environments
+Settings.Coolant.FuelPumpReturn = 200;
+Settings.Coolant.FuelPumpSink = 500;
+Settings.Coolant.AmbientPumpReturn = 273 - 100;
+Settings.Coolant.AmbientPumpSink = 273 + 300;
+Settings.Coolant.Fuel = 250;
+Settings.Coolant.Ambient = 273 - 40;
+Settings.MaxTemperature.Battery = 40 + 273;
+Settings.MaxTemperature.Motor = 273 + 95;
+Settings.MaxTemperature.Generator = 95 + 273;
+
+ThermalSystemA.Settings = Settings;
+ThermalSystemP.Settings = Settings;
+
+powers = linspace(1,5e5,20);
+
+passivemass = zeros(1,length(powers));
+
+activemass = zeros(2,length(powers));
+
+% Run loops to calculate for various powers
+for ii = 1:length(powers)
+    ThermalSystemP.Propulsion.MaxPower = ...
+        powers(ii) * ones(length(ThermalSystemP.CompNames),1); % Fake numbers for now
+    % Size the mass flow internal to the connections between the components
+    ThermalSystemP = ThermalPkg.MassFlowSizing(ThermalSystemP);
+    passivemass(ii) = ThermalSystemP.Loops.MassFlow;
+
+    ThermalSystemA.Propulsion.MaxPower = ...
+        powers(ii) * ones(length(ThermalSystemA.CompNames),1); % Fake numbers for now
+    % Size the mass flow internal to the connections between the components
+    ThermalSystemA = ThermalPkg.MassFlowSizing(ThermalSystemA);
+    activemass(:,ii) = ThermalSystemA.Loops.MassFlow;
+
+end
+
+
+% Inspect some outputs
+% Mass = ThermalSystemP.Loops.MassFlow
+RegalBlue      = [0,     0.251, 0.478]; % Darker Blue
+Astral         = [0.180, 0.459, 0.714]; % Medium Blue
+Cornflower     = [0.580, 0.745, 0.894]; % Light Blue
+AlgaeGreen     = [0.475, 0.855, 0.624]; % Light Green
+JungleGreen    = [0.169, 0.655, 0.459]; % Dark Green
+GuardsmanRed   = [0.753, 0,     0    ]; % Red (Warm Accent)
+Jaffa          = [0.930, 0.490, 0.192]; % Orange (Warm Accent)
+Corn           = [0.918, 0.722, 0    ]; % Yellow (Warm Accent)
+Midnight       = [0,     0.086, 0.165]; % Very Dark (Nearly Black) Blue
+SilverChalice  = [0.686, 0.671, 0.671]; % Gray
+BlueRibbon     = [0.086, 0.404, 0.945]; % Rich Blue (Cool Accent)
+DodgerBlue     = [0.235, 0.553, 0.984]; % Medium Rich Blue (Cool Accent)
+Malibu         = [0.365, 0.777, 0.996]; % Lighter Blue (Cool Accent)
+AquaMarine     = [0.377, 0.980, 0.863]; % Cyan (Cool Accent)
+ElectricViolet = [0.624, 0.067, 0.757]; % Purple (Cool Accent)
+
+
+figure(3)
+plot(powers,activemass(1,:),'linewidth',2,'color',RegalBlue)
+hold on
+plot(powers,activemass(2,:),'linewidth',2,'color',JungleGreen)
+plot(powers,passivemass,'linewidth',2,'color',GuardsmanRed)
+grid on
+
+ylabel("Loop Mass Flow (kg/s)")
+xlabel("Heat Generated by Source (W)")
+legend('Active: Source-Pump','Active: Pump-Sink','Passive','location','northwest')
+
+ax = gca;
+ax.FontName = 'times';
+ax.FontSize = 16;
+
+set(gcf,'Position',[100 100 820 500])
+
 
 
 
